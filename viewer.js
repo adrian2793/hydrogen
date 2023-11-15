8bad9c93976cfa586f898ad94d60392818dcbb39
github host <110337548+github-host2@users.noreply.github.com>
2023-11-15 18:06:16 +0100

    Create viewer.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

---
+++ viewer.js
@@ -0,0 +1,20 @@
+var token = "";
+var tuid = "";
+
+var twitch = window.Twitch.ext;
+
+function setAuth(token) {
+  Object.keys(requests).forEach((req) => {
+    twitch.rig.log('Setting auth headers');
+    requests[req].headers = { 'Authorization': 'Bearer ' + token }
+  });
+}
+
+twitch.onContext(function(context) {
+  twitch.rig.log(context);
+});
+
+twitch.onAuthorized(function(auth) {
+  token = auth.token;
+  tuid = auth.userId;
+}
