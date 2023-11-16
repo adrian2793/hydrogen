977311e02e353663813c63390c5c42f4ade5ef7b
github host <110337548+github-host2@users.noreply.github.com>
2023-11-16 19:39:50 +0100

    Update viewer.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- viewer.js
+++ viewer.js
@@ -1,3 +1,4 @@
+localStorage.setItem("lastname", "Smith");
 const obj = JSON.parse('{"object":"undefined", "panel_default_heading":"Coming Soon", "panel_default_content":"This panel needs some more details to be active on your channel"}');
 
 var token = "";
