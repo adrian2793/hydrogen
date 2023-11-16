b7aa9dc9255a8e1a1e600bc364d5c9f1192b5884
github host <110337548+github-host2@users.noreply.github.com>
2023-11-16 16:51:18 +0100

    Update viewer.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- viewer.js
+++ viewer.js
@@ -5,8 +5,8 @@ var twitch = window.Twitch.ext;
 
 function setAuth(token) {
   Object.keys(requests).forEach((req) => {
-      twitch.rig.log('Setting auth headers');
-      requests[req].headers = { 'Authorization': 'Bearer ' + token }
+    twitch.rig.log('Setting auth headers');
+    requests[req].headers = { 'Authorization': 'Bearer ' + token }
   });
 }
 
@@ -15,28 +15,11 @@ twitch.onContext(function(context) {
 });
 
 twitch.onAuthorized(function(auth) {
-  // save our credentials
   token = auth.token;
   tuid = auth.userId;
-
-  // enable the button
-  $('#cycle').removeAttr('disabled');
-
   setAuth(token);
 });
 
-function updateBlock(hex) {
-    $('#color').css('background-color', hex);
+function parse(id, object) {
+  document.getElementById("demo").innerHTML = obj.object;
 }
-
-function getRandomColor() {
-  return '#'+Math.floor(Math.random()*16777215).toString(16);
-}
-
-$(function() {
-    $('#cycle').prop('disabled', false);
-
-    $('#cycle').click(function() {
-      updateBlock(getRandomColor());
-    });
-});
