815c26ef2edcb0b37418aae6fc83b89967f759cb
github host <110337548+github-host2@users.noreply.github.com>
2023-11-16 20:20:08 +0100

    Update viewer.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- viewer.js
+++ viewer.js
@@ -1,23 +1,5 @@
 const obj = JSON.parse('{"object":"undefined", "panel_default_heading":"Coming Soon", "panel_default_content":"This panel needs some more details to be active on your channel"}');
 
-var token = auth.token;
-var tuid = "";
-
-var twitch = window.Twitch.ext;
-
-function setAuth(token) {
-  Object.keys(requests).forEach((req) => {
-    twitch.rig.log('Setting auth headers');
-    requests[req].headers = { 'Authorization': 'Bearer ' + token }
-  });
-}
-
-twitch.onContext(function(context) {
-  twitch.rig.log(context);
-});
-
-twitch.onAuthorized(function(auth) {
-  token = auth.token;
-  tuid = auth.userId;
-  setAuth(token);
+window.Twitch.ext.onAuthorized((auth) => {
+    console.log('got auth');
 });
