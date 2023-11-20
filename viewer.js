3849c57d17f0e3a7356655aca90a0a645cf744b7
github host <110337548+github-host2@users.noreply.github.com>
2023-11-20 13:34:42 +0100

    Update viewer.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- viewer.js
+++ viewer.js
@@ -1,9 +1,5 @@
 const obj = JSON.parse('{"object": "undefined", "panel_heading": "Hydrogen", "panel_content": "Customize this panel as Streamer in the settings", "live_config_heading": "Live Settings", "config_developer_mode_toogle_label_content": "&nbsp;&nbsp;Enable Developer Mode", "config_analytics_toogle_label_content": "&nbsp;&nbsp;Enable Analytics", "config_heading": "Settings"}');
 
-encrypt("config_heading", "config_heading");
-encrypt("config_developer_mode_toogle_label", "config_developer_mode_toogle_label_content");
-encrypt("config_analytics_toogle_label", "config_analytics_toogle_label_content");
-
 var twitch = window.Twitch.ext;
 
 function setAuth(token) {
@@ -26,3 +22,7 @@ twitch.onAuthorized(function(auth) {
 function encrypt(id, object) {
   document.getElementById(id).innerHTML = obj[object];
 }
+
+encrypt("config_heading", "config_heading");
+encrypt("config_developer_mode_toogle_label", "config_developer_mode_toogle_label_content");
+encrypt("config_analytics_toogle_label", "config_analytics_toogle_label_content");
