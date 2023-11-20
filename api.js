b7884f0e576d0ec1d7d5811180c1697e91d3671e
github host <110337548+github-host2@users.noreply.github.com>
2023-11-20 21:40:47 +0100

    Update api.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- api.js
+++ api.js
@@ -1,5 +1,13 @@
 const http_headers = new Headers();
+const patch_vacation_schedule_options = {
+  method: "PATCH",
+  headers: http_headers,
+};
 
-function set_header(name, value) {
+function set_headers(name, value) {
   http_headers.set(name, value);
+}
+
+function patch_vacation_schedule() {
+  new Request("https://api.twitch.tv/helix/schedule/settings?broadcaster_id=123456", patch_vacation_schedule_options);
 }
