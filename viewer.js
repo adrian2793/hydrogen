2f6eb98787cffb2f6c89e04134e13d613f9588a2
github host <110337548+github-host2@users.noreply.github.com>
2023-11-20 19:18:57 +0100

    Update viewer.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- viewer.js
+++ viewer.js
@@ -1,4 +1,4 @@
-const obj = JSON.parse('{"object": "undefined", "panel_heading": "Hydrogen", "panel_content": "Customize this panel as Streamer in the settings", "live_config_heading": "Live Settings", "config_developer_mode_toogle_label_content": "&nbsp;&nbsp;Enable Developer Mode", "config_analytics_toogle_label_content": "&nbsp;&nbsp;Enable Analytics", "config_heading": "Settings"}');
+const obj = JSON.parse('{"object": "undefined", "panel_heading": "Hydrogen", "panel_content": "Customize this panel as Streamer in the settings", "live_config_heading": "Live Settings", "config_developer_mode_toogle_label_content": "&nbsp;&nbsp;Enable Developer Mode", "config_analytics_toogle_label_content": "&nbsp;&nbsp;Enable Analytics", "config_heading": "Settings", "config_save_button_content_saved": "Saved"}');
 
 var twitch = window.Twitch.ext;
 
@@ -36,7 +36,7 @@ addEventListener("DOMContentLoaded", (event) => {
 function authorizeUser() {
   var hash = location.hash;
   if(hash.includes("#access_token") == true) {
-    alert("Big men");
+    encrypt("config_save_button", "config_save_button_content_saved")
   } else {
     window.location.assign("https://id.twitch.tv/oauth2/authorize?force_verify=false&response_type=token&client_id=94mse963yladye0qb4qcwrbadbhckc&redirect_uri=https%3A//hydrogen-extension.netlify.app/config.html&scope=user%3Aread%3Aemail");
     var hash = location.hash;
