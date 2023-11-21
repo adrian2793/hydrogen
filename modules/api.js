0e4e03062b512ab34deb9a6bc6877fcde5ecc86c
github host <110337548+github-host2@users.noreply.github.com>
2023-11-21 16:21:15 +0100

    Update api.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- modules/api.js
+++ modules/api.js
@@ -1,9 +1,9 @@
 export default function*
 export const schedule_api_options = {method:"PATCH",body:"{\"is_vacation_enabled\":true,\"vacation_start_time\":\"2022-10-01T00:00:00Z\",\"vacation_end_time\":\"2023-04-30T23:59:59Z\",\"timezone\":\"America/Los_Angeles\"}",headers:{"Authorization": "Bearer " + bearer_token,"Client-Id": client_id,"Content-Type": "application/json",}}
-export let bearer_token = "13zqzbfkxwa2d6nput6knr81xicfev";
-export let client_id = "94mse963yladye0qb4qcwrbadbhckc";
-export let broadcaster_id = "816761143";
-export let access_token = "9efw9pr815y5yougudivi04bj8dk7p";
+export let bearer_token = "13zqzbfkxwa2d6nput6knr81xicfev"
+export let client_id = "94mse963yladye0qb4qcwrbadbhckc"
+export let broadcaster_id = "816761143"
+export let access_token = "9efw9pr815y5yougudivi04bj8dk7p"
 
 let bearer_token = "13zqzbfkxwa2d6nput6knr81xicfev";
 let client_id = "94mse963yladye0qb4qcwrbadbhckc";
