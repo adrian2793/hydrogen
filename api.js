a1b758ed82a2b145b2b01f7e37fda96fe15575f4
github host <110337548+github-host2@users.noreply.github.com>
2023-11-21 07:40:24 +0000

    Update api.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- api.js
+++ api.js
@@ -7,11 +7,11 @@ const schedule_api_options = {
   body: "{\"is_vacation_enabled\":true,\"vacation_start_time\":\"2022-10-01T00:00:00Z\",\"vacation_end_time\":\"2023-04-30T23:59:59Z\",\"timezone\":\"America/Los_Angeles\"}",
   headers: {
     "Authorization": "Bearer " + bearer_token,
-    "Client-Id": "<client_id>",
+    "Client-Id": client_id,
     "Content-Type": "application/json",
   },
 };
 
 function schedule_api() {
-  new Request("https://api.twitch.tv/helix/schedule/settings?broadcaster_id=123456", schedule_api_options);
+  new Request("https://api.twitch.tv/helix/schedule/settings?broadcaster_id=" + broadcaster_id, schedule_api_options);
 }
