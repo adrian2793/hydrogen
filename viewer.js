3e8ab2a42ebfda6c44d32f196bbc5027c838a8ec
github host <110337548+github-host2@users.noreply.github.com>
2023-11-21 09:19:51 +0100

    Update viewer.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- viewer.js
+++ viewer.js
@@ -1,4 +1,4 @@
-import "./api.js";
+import "/modules/api.js";
 
 const obj = JSON.parse('{"object": "undefined", "panel_heading": "Hydrogen", "panel_content": "Customize this panel as Streamer in the settings", "live_config_heading": "Live Settings", "config_developer_mode_toogle_label_content": "&nbsp;&nbsp;Enable Developer Mode", "config_analytics_toogle_label_content": "&nbsp;&nbsp;Enable Analytics", "config_heading": "Settings", "config_save_button_content_saved": "Saved", "config_save_button_content": "Save"}');
 
