b09587a3dde08df7dd23d74a6eb80dbd4e0b1868
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-11-22 12:56:13 +0000

    Update api.js
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- api.js
+++ api.js
@@ -18,7 +18,7 @@ function schedule_api() {
   alert(Request);
 }
 
-export default function* schedule_api()
+export default function* ()
 export bearer_token
 export client_id
 export broadcaster_id
