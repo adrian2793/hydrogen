1974f0700cdcda4c69fde52dd6993fb02e93dad4
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-11-22 16:40:44 +0100

    Update api.js
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- modules/api.js
+++ modules/api.js
@@ -1,9 +1,9 @@
-let bearer_token = "13zqzbfkxwa2d6nput6knr81xicfev";
-let client_id = "94mse963yladye0qb4qcwrbadbhckc";
-let broadcaster_id = "816761143";
-let access_token = "9efw9pr815y5yougudivi04bj8dk7p";
+export let bearer_token = "13zqzbfkxwa2d6nput6knr81xicfev";
+export let client_id = "94mse963yladye0qb4qcwrbadbhckc";
+export let broadcaster_id = "816761143";
+export let access_token = "9efw9pr815y5yougudivi04bj8dk7p";
 
-const schedule_api_options = {
+export const schedule_api_options = {
   method: "PATCH",
   body: "{\"is_vacation_enabled\":true,\"vacation_start_time\":\"2022-10-01T00:00:00Z\",\"vacation_end_time\":\"2023-04-30T23:59:59Z\",\"timezone\":\"America/Los_Angeles\"}",
   headers: {
@@ -17,9 +17,3 @@ export default function schedule_api() {
   new Request("https://api.twitch.tv/helix/schedule/settings?broadcaster_id=" + broadcaster_id, schedule_api_options);
   alert(Request);
 }
-
-export bearer_token
-export client_id
-export broadcaster_id
-export access_token
-export schedule_api_options
