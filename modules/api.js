b29c3fd4ca6404434a5b7e17e9ab4fa3688d8319
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-11-23 14:43:44 +0100

    Update api.js
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- modules/api.js
+++ modules/api.js
@@ -15,5 +15,5 @@ export const schedule_api_options = {
 
 export function schedule_api() {
   new Request("https://api.twitch.tv/helix/schedule/settings?broadcaster_id=" + broadcaster_id, schedule_api_options);
-  alert(Request);
+  alert(response.status);
 }
