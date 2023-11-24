7b9449eca2791fafeac029651af595957934d617
Adrian <76214766+adrian2793@users.noreply.github.com>
2023-11-24 11:39:59 +0100

    Update api.js
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- modules/api.js
+++ modules/api.js
@@ -5,7 +5,7 @@ export let access_token = "9efw9pr815y5yougudivi04bj8dk7p";
 
 export const schedule_api_options = {
   method: "PATCH",
-  body: "{\"is_vacation_enabled\":true,\"vacation_start_time\":\"2025-10-01T00:00:00Z\",\"vacation_end_time\":\"2029-04-30T23:59:59Z\",\"timezone\":\"America/Los_Angeles\"}",
+  body: '{"is_vacation_enabled":true,"vacation_start_time":"2025-10-01T00:00:00Z","vacation_end_time":"2029-04-30T23:59:59Z","timezone":"America/Los_Angeles"}',
   headers: {
     "Authorization": "Bearer " + bearer_token,
     "Client-Id": client_id,
