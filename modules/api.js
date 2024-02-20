23c1b46d12910aaac06b1d7cae6ff3b394335dbe
Adrian <76214766+adrian2793@users.noreply.github.com>
2024-02-20 13:38:59 +0100

    Update api.js
    Signed-off-by: Adrian <76214766+adrian2793@users.noreply.github.com>

--- modules/api.js
+++ modules/api.js
@@ -5,7 +5,6 @@ export let access_token = "";
 
 export const schedule_api_options = {
   method: "PATCH",
-  body: '{"is_vacation_enabled":true,"vacation_start_time":"2025-10-01T00:00:00Z","vacation_end_time":"2029-04-30T23:59:59Z","timezone":"America/Los_Angeles"}',
   headers: {
     "Authorization": "Bearer " + bearer_token,
     "Client-Id": client_id,
@@ -14,7 +13,7 @@ export const schedule_api_options = {
 };
 
 export function schedule_api() {
-  const request = new Request("https://api.twitch.tv/helix/schedule/settings?broadcaster_id=" + broadcaster_id, schedule_api_options);
+  const request = new Request("https://api.twitch.tv/helix/schedule/settings?broadcaster_id=" + broadcaster_id + "&is_vacation_enabled=true&vacation_start_time=2025-10-01T00:00:00Z&vacation_end_time=2029-04-30T23:59:59Z&timezone=America/Los_Angeles", schedule_api_options);
   fetch(request)
   .then((response) => {
     if (response.status === 200) {
